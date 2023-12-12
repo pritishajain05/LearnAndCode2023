5311cfea7e5517eb4d633cbf96b98657b1142c2b
pritishajain05 <142470297+pritishajain05@users.noreply.github.com>
2023-12-12 09:29:56 +0530

    Delete EmailCarbonFootPrint directory

--- EmailCarbonFootPrint/EmailCarbonFootprintOutput.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "EmailData.h"
-
-class EmailCarbonFootprintOutput {
-public:
-    static void displayCarbonFootprint(const EmailData& emailData);
-};
--- EmailCarbonFootPrint/InputValidator.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-using namespace std;
-
-class InputValidator {
-public:
-    static int getIntInput(const string& prompt);
-};
